c7f81a4f216e242d54c09e655e38d6eda1230a1d
Eztekto <88131909+Eztekto@users.noreply.github.com>
2021-07-30 13:12:27 +0300

    Update Main.java

--- task_2/task_2.2/Main.java
+++ task_2/task_2.2/Main.java
@@ -1,5 +1,5 @@
 public class Main {
-    public static void main(String... args) {
+    public static void main(String[] args) {
         for (int a = 1; a < 11; a++) {
             for (int b = 1; b < 11; b++) {
                 System.out.print(a * b + " ");
