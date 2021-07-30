64650306e10c6ea5ae1306e8366164e3003c9806
Eztekto <88131909+Eztekto@users.noreply.github.com>
2021-07-30 12:56:07 +0300

    Update Main.java

--- task_2/task_2.3/Main.java
+++ task_2/task_2.3/Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        for (int a = 0; a < 11; a++) {
+        for (int a = 1; a < 11; a++) {
             for (int b = 0; b < a; b++) {
                 System.out.print(8);
             }
-            System.out.println();
+                System.out.println();
         }
     }
 }
