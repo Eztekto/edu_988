a4664ce73dba4bba4de3c5bcc1aa8bd2a5fdb18d
Eztekto <88131909+Eztekto@users.noreply.github.com>
2021-07-30 13:11:15 +0300

    Update Main.java

--- task_2/task_2.2/Main.java
+++ task_2/task_2.2/Main.java
@@ -1,10 +1,10 @@
 public class Main {
-    public static void main(String[] args) {
-        for (int a = 0; a < 11; a++) {
-            for (int b = 0; b < a; b++) {
-                System.out.print(8);
+    public static void main(String... args) {
+        for (int a = 1; a < 11; a++) {
+            for (int b = 1; b < 11; b++) {
+                System.out.print(a * b + " ");
             }
-            System.out.println();
+            System.out.println("");
         }
     }
 }
