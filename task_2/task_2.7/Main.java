a155ed79d71371582155a47655f334a319485789
Eztekto <88131909+Eztekto@users.noreply.github.com>
2021-07-30 19:08:05 +0300

    Update Main.java

--- task_2/task_2.7/Main.java
+++ task_2/task_2.7/Main.java
@@ -6,13 +6,11 @@ public class Main {
         System.out.println("Введите число: ");
         int a = scanner.nextInt();
         int index = 1;
+        String result = "False";
         for (int b = 2; b < a; b++) {
-            if (a % b != 0)
-                System.out.println(++index + ". False");
-            else
-                System.out.println(++index + ". True");
+            if (a % b == 0)
+                result = "True";
         }
+        System.out.println(result);
     }
 }
-// Я пытался сделать так, чтобы он выводил только один раз результат, но так выходило, что если ставить команду break, то он выдывал только результат деления на 2.
-// Как сделать так, чтобы он перебирал результаты, и выдавал только True, если там есть хотя бы один такой результат я не дошёл.
