bfac5525dff6169bb71d33c698ebb048988e59e7
Eztekto <88131909+Eztekto@users.noreply.github.com>
2021-07-30 11:19:28 +0300

    Create Main.java

---
+++ task_2/task_2.1/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        String[] strbox = {"Мама", "Мыла", "Раму"};
+        int index = 0;
+        int position = 0;
+        for (int i = 0; i < strbox.length(); i++)
+            for (int j = 0; j < strbox.length(); j++)
+                for (int k = 0; k < strbox.length(); k++)
+                    if (i != j && j != k && k != i)
+                        System.out.println(strbox(i), strbox(j), strbox(k));
+    }
+}
