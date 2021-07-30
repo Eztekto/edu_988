6176859fc5b0f1d792c35f42e366fa76fb4429e9
Eztekto <88131909+Eztekto@users.noreply.github.com>
2021-07-30 17:27:19 +0300

    Create Main.java

---
+++ task_2/task_2.8/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int result = 1;
+        int[] nums = {2, 3, 4, 5};
+        for (int i = 0; i < nums.length; i++){
+            result = result * nums[i];
+        }
+        System.out.print(result);
+           }
+    }
