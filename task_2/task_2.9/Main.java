3b3544301c8926930eb07136f5f6f623133280ae
Eztekto <88131909+Eztekto@users.noreply.github.com>
2021-07-30 18:31:15 +0300

    Create Main.java

---
+++ task_2/task_2.9/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] nums = {2, 3, 4, 5};
+
+        for (int i : nums){
+            if (i == 5)
+                System.out.println("Да");
+
+            else
+                System.out.println("Нет");
+        }
+
+           }
+    }
+
