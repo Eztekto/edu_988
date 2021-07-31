d79ded488844a7d6d9406a793f1b2ec8ddd87fe3
Eztekto <88131909+Eztekto@users.noreply.github.com>
2021-07-31 15:27:34 +0300

    Create Main.java

---
+++ task_2/task_2.11/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int sum = 0;
+        String result = "Попыток " + 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+            if (sum > 10)
+            result = "Попыток " + (i - 1);
+        }
+        System.out.println(result);
+    }
+}
