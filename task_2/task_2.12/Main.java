93bddba8cc89784879f19156b3716f7c14812939
Eztekto <88131909+Eztekto@users.noreply.github.com>
2021-08-01 23:10:35 +0300

    Update Main.java

--- task_2/task_2.12/Main.java
+++ task_2/task_2.12/Main.java
@@ -5,16 +5,19 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         int[] arr = new int[10];
         for (int i = 0; i < 10; i++) {
+            System.out.print("Введите число: ");
             arr[i] = scanner.nextInt();
         }
         int sum = 1, count = 0;
         for (int i = 1; i < arr.length; i++) {
-            if (arr[i] == arr[i - 1])
+            if (arr[i] == arr[i - 1]) {
                 sum++;
-            if (sum > count) {
-                count = sum;
-            } else sum = 1;
+                if (sum > count) {
+                    count = sum;
+                }
+            }else sum = 1;
         }
-        System.out.println(count);
+
+            System.out.println(count);
+        }
     }
-}
