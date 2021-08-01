ffa282cb18572560558ed7438104bae51d334481
Eztekto <88131909+Eztekto@users.noreply.github.com>
2021-08-01 22:54:37 +0300

    Create Main.java

---
+++ task_2/task_2.12/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] arr = new int[10];
+        for (int i = 0; i < 10; i++) {
+            arr[i] = scanner.nextInt();
+        }
+        int sum = 1, count = 0;
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] == arr[i - 1])
+                sum++;
+            if (sum > count) {
+                count = sum;
+            } else sum = 1;
+        }
+        System.out.println(count);
+    }
+}
