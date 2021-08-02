a3671725f79e8fc14e6d56b52895d480583a4b87
Eztekto <88131909+Eztekto@users.noreply.github.com>
2021-08-03 02:35:02 +0300

    Create Main.java

---
+++ task_3/task_3.5/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static int min (int a) {
+       return (a);
+    }
+public static void main(String[] args) {
+        int num[] = {4,2,6,2,65};
+        System.out.println(min(num[num.length - 1]));
+        System.out.println(min(num[num.length - 2]));
+        System.out.println(min(num[num.length - 3]));
+        System.out.println(min(num[num.length - 4]));
+        System.out.println(min(num[num.length - 5]));
+    }
+}
