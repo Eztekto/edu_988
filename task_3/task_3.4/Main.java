39c9084201ffa0bbe5456781f6f651c021ba985a
Eztekto <88131909+Eztekto@users.noreply.github.com>
2021-08-03 01:16:01 +0300

    Create Main.java

---
+++ task_3/task_3.4/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+    public static boolean equal(int a, int b) {
+        int x = a + b;
+        if (x > 10)
+            return true;
+        else
+            return false;
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(equal(1, 2));
+            }
+}
