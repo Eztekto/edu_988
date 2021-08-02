639e57ec1aa9dd15b393602a0e6ae890653933d5
Eztekto <88131909+Eztekto@users.noreply.github.com>
2021-08-02 23:20:54 +0300

    Create Main.java

---
+++ task_3/task_3.1/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static int min(int a, int b, int c) {
+        int min = a;
+                if (b <= a && b <= c)
+            min = b;
+                if (c <= a && c <= b)
+            min = c;
+        return min;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(min(1, 2, 3));
+        System.out.println(min(-1, -2, -3));
+        System.out.println(min(3, 5, 3));
+        System.out.println(min(5, 5, 10));
+    }
+}
