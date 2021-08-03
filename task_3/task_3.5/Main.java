ee02061a4b4e3103ff38c6457d07a7eb9b71793d
Eztekto <88131909+Eztekto@users.noreply.github.com>
2021-08-03 20:17:20 +0300

    Update Main.java

--- task_3/task_3.5/Main.java
+++ task_3/task_3.5/Main.java
@@ -1,14 +1,17 @@
 public class Main {
-
-    public static int min (int a) {
-       return (a);
-    }
-public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         int num[] = {4,2,6,2,65};
-        System.out.println(min(num[num.length - 1]));
-        System.out.println(min(num[num.length - 2]));
-        System.out.println(min(num[num.length - 3]));
-        System.out.println(min(num[num.length - 4]));
-        System.out.println(min(num[num.length - 5]));
+
+        reverse(num,0);
+    }
+
+    public static void reverse(int[] arr, int position){
+        if(position == arr.length){
+            return;
+        }
+
+        reverse(arr, position + 1);
+        System.out.println(arr[position]);
     }
 }
