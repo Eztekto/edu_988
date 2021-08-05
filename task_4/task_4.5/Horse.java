d85b9b7847eff5f9b6a10d1ead560a3b3e1a9948
Eztekto <88131909+Eztekto@users.noreply.github.com>
2021-08-05 19:05:23 +0300

    Create Horse.java

---
+++ task_4/task_4.5/Horse.java
@@ -0,0 +1,14 @@
+public class Horse extends Animal {
+    String breed;
+    String color;
+
+    public Horse(boolean veg, String speak, byte paw, String breed, String color) {
+        super(veg, speak, paw);
+        this.breed = breed;
+        this.color = color;
+    }
+   void run() {
+        System.out.println(speak + ", я поскакал");
+    }
+
+}
