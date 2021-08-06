3e0c842f947ccc4d2ddbe17c08c2b3e6f6d04833
Eztekto <88131909+Eztekto@users.noreply.github.com>
2021-08-07 00:12:47 +0300

    Create Main.java

---
+++ task_5/task_5.3/Main.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Integer> number = new ArrayList(Arrays.asList(17,15,12,13,14));
+            number.add(1);
+            number.add(2);
+            number.add(3);
+            number.add(4);
+            number.add(5);
+            number.add(6);
+            number.add(7);
+            number.add(8);
+            number.add(9);
+            System.out.print(number);
+
+        }
+    }
